33ae5239cc3aa5b54c17bb0e3d06a63754ceb495
JaeMinHw <95284552+JaeMinHw@users.noreply.github.com>
2023-06-07 01:01:34 +0900

    Add files via upload

--- client.py
+++ client.py
@@ -31,38 +31,41 @@ def connect() :
 def order(data):
     print('Received response:',data)
     move(data)
-    cal_distance()
+    # https://blog.naver.com/PostView.nhn?blogId=simjk98&logNo=221223898102
+    sen1= Cal_distance()
+    sen1.cal_distance(TRIGER,ECHO)
 
 
-    
+# https://hemon.tistory.com/72
+class Cal_distance:
+    def cal_distance(self,TRIGER,ECHO) :
+        try:
+            while True:
+                gpio.output(TRIGER, gpio.LOW)
+                time.sleep(0.1)
+                gpio.output(TRIGER, gpio.HIGH)
+                time.sleep(0.00002)
+                gpio.output(TRIGER, gpio.LOW)
 
-def cal_distance() :
-    try:
-        while True:
-            gpio.output(TRIGER, gpio.LOW)
-            time.sleep(0.1)
-            gpio.output(TRIGER, gpio.HIGH)
-            time.sleep(0.00002)
-            gpio.output(TRIGER, gpio.LOW)
+                while gpio.input(ECHO) == gpio.LOW:
+                    startTime = time.time()   # 1sec unit
 
-            while gpio.input(ECHO) == gpio.LOW:
-                startTime = time.time()   # 1sec unit
+                while gpio.input(ECHO) == gpio.HIGH:
+                    endTime = time.time()
 
-            while gpio.input(ECHO) == gpio.HIGH:
-                endTime = time.time()
+                period = endTime - startTime
+                dist1 = round(period * 1000000 / 58, 2)
+                dist2 = round(period * 17241, 2)
 
-            period = endTime - startTime
-            dist1 = round(period * 1000000 / 58, 2)
-            dist2 = round(period * 17241, 2)
-
-            print("Dist1", dist1, "cm", ", Dist2", dist2, "cm")
+                print("Dist1", dist1, "cm", ", Dist2", dist2, "cm")
+                
+                if dist1 < 10 and dist2 < 10:
+                    move("stop")
+                    print("detect")
+                    self.join()
             
-            if dist1 < 10 and dist2 < 10:
-                move("stop")
-                print("detect")
-        
-    except:
-        gpio.cleanup()  
+        except:
+            gpio.cleanup()  
 
 
 def move(data) :
