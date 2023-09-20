082871ad778a958413808aabfcfa9e61609fbd7a
JaeMinHw <95284552+JaeMinHw@users.noreply.github.com>
2023-09-20 16:11:26 +0900

    camera total test and thread manage
    camera total test and thread manage

--- client.py
+++ client.py
@@ -8,37 +8,16 @@ import os, sys
 import asyncio
 import requests
 from ast import literal_eval
+import aiohttp
+port = "/dev/ttyACM0"
+ard = serial.Serial(port,9600)
 
 import RPi.GPIO as gpio
 import time
 
-import cv2
-import asyncio
-import aiohttp
-import base64
-
-# 로컬 카메라 캡처 객체 생성
-cap = cv2.VideoCapture(0)  # 0은 기본 카메라를 의미합니다. 다른 카메라를 사용하려면 숫자를 변경하세요.
-
-# 서버 엔드포인트 URL 설정
-server_url = "http://3.36.70.207:5000/upload"  # 서버 URL을 적절하게 변경하세요.
-
-# 서버로 전송할 데이터 형식 설정 (이미지를 base64로 인코딩하여 전송)
-headers = {"Content-Type": "application/json"}
-
-# 이미지 크기와 압축 설정
-IMAGE_WIDTH = 320
-IMAGE_HEIGHT = 240
-JPEG_QUALITY = 50
-
-# 카메라 해상도 설정
-cap.set(3, IMAGE_WIDTH)
-cap.set(4, IMAGE_HEIGHT)
-
-port = "/dev/ttyACM1"
-ard = serial.Serial(port,9600)
-
-
+import io
+import numpy as np
+from PIL import Image
  
 TRIGER = 24
 ECHO = 23
@@ -53,18 +32,17 @@ TRIGER4 = 25
 ECHO4 = 17  
 
  
-gpio.setmode(gpio.BCM)
-gpio.setup(TRIGER, gpio.OUT)
-gpio.setup(ECHO, gpio.IN)
+
+
 
 
 sio = socketio.Client()
 
+# thread1 = threading.Thread()
+# thread2 = threading.Thread()
+# thread3 = threading.Thread()
+# thread4 = threading.Thread()
 
-thread1 = None
-thread2 = None
-thread3 = None
-thread4 = None
 
 
 # after distance detect, if distance is too close, s = '7' ard.write(s.encode()) 
@@ -75,71 +53,88 @@ thread4 = None
 def connect() :
     print('Connect to server')
     
-    
-
+thread1 = None
+thread2 = None
+thread3 = None
+thread4 = None
+x = None
+y= None
+z = None
+k = None
 @sio.on('order')
 def order(data):
-    print('Received response:',data)
-    move(data)
-    x = Cal_distance()
-    y = Cal_distance()
-    z = Cal_distance()
-    k = Cal_distance()
-    thread1 = threading.Thread(target=x.cal_distance,args = ("t1",TRIGER,ECHO))
-    thread2 = threading.Thread(target=y.cal_distance,args=("t2",TRIGER2,ECHO2))
-    thread3 = threading.Thread(target=z.cal_distance,args = ("t3",TRIGER3,ECHO3))
-    thread4 = threading.Thread(target=k.cal_distance,args = ("t4",TRIGER4,ECHO4))
+    
 
+    print('Received response:', data)
+    move(data)
+    
+    global x,y,z,k
+    x = Cal_distance("t1", TRIGER, ECHO)
+    y = Cal_distance("t2", TRIGER2, ECHO2)
+    z = Cal_distance("t3", TRIGER3, ECHO3)
+    k = Cal_distance("t4", TRIGER4, ECHO4)
+
+    global thread1, thread2, thread3, thread4
+    thread1 = threading.Thread(target=x.cal_distance)
+    thread2 = threading.Thread(target=y.cal_distance)
+    thread3 = threading.Thread(target=z.cal_distance)
+    thread4 = threading.Thread(target=k.cal_distance)
 
     thread1.start()
     thread2.start()
     thread3.start()
     thread4.start()
 
+class Cal_distance:
+    def __init__(self, name, TRIGER, ECHO):
+        self.name = name
+        self.TRIGER = TRIGER
+        self.ECHO = ECHO
+        self.dist1 = 0
+        self.stop_event = threading.Event()
 
-
-class Cal_distance :
-    def cal_distance(self,name,TRIGER,ECHO) :
-
- 
+    def cal_distance(self):
         gpio.setmode(gpio.BCM)
-        gpio.setup(TRIGER, gpio.OUT)
-        gpio.setup(ECHO, gpio.IN)
-        
-        #period = endTime - startTime에서 startTime값이 null인 경우 발생->초기화
-        startTime = time.time()  
-        
+        gpio.setup(self.TRIGER, gpio.OUT)
+        gpio.setup(self.ECHO, gpio.IN)
+
         try:
-            while True:
-                gpio.output(TRIGER, gpio.LOW)
+            while not self.stop_event.is_set():
+                startTime = time.time()
+                gpio.output(self.TRIGER, gpio.LOW)
                 time.sleep(0.1)
-                gpio.output(TRIGER, gpio.HIGH)
+                gpio.output(self.TRIGER, gpio.HIGH)
                 time.sleep(0.00002)
-                gpio.output(TRIGER, gpio.LOW)
-        
-                while gpio.input(ECHO) == gpio.LOW:
-                    startTime = time.time()   # 1sec unit
-        
-                while gpio.input(ECHO) == gpio.HIGH:
+                gpio.output(self.TRIGER, gpio.LOW)
+
+                while gpio.input(self.ECHO) == gpio.LOW:
+                    startTime = time.time()
+
+                while gpio.input(self.ECHO) == gpio.HIGH:
                     endTime = time.time()
-        
+
                 period = endTime - startTime
-                dist1 = round(period * 1000000 / 58, 2)
-                dist2 = round(period * 17241, 2)
-        
-                print( "Dist1", dist1, "cm", ", Dist2", dist2, "cm")
+                self.dist1 = round(period * 1000000 / 58, 2)
+
                 
-                if dist1 < 10 and dist2 < 10:
+
+                if self.dist1 < 30:
+                    print("", self.name, "Dist1", self.dist1, "cm")
                     move("stop")
-        except:
-            gpio.cleanup()
+                    requests.get('http://3.36.70.207/status/',self.name)
 
 
-
+        except Exception as e:
+            print("Error:", str(e))
+            
+            
+    def stop(self):
+        self.stop_event.set()  # stop_event 설정하여 쓰레드 종료
 
 
 def move(data) :
     if data == 'go':
+        gpio.setmode(gpio.BCM)
         s = '1'
         print("let's get it")
         ard.write(s.encode())
@@ -163,19 +158,56 @@ def move(data) :
         ard.write(s.encode())
     elif data == 'stop':
         s = '7'
+        print("2")
         ard.write(s.encode())
-        thread1.join()
-        thread2.join()
-        thread3.join()
-        thread4.join()
+        s1 = '0'
+        ard.write(s1.encode())
+        
+        # thread1.join()
+        # thread2.join()
+        # thread3.join()
+        # thread4.join()
+        # gpio.ouput(TRIGER,gpio.LOW)
+        # gpio.ouput(TRIGER2,gpio.LOW)
+        # gpio.ouput(TRIGER3,gpio.LOW)
+        # gpio.ouput(TRIGER4,gpio.LOW)
+    if x is not None :    
+        print("sad")
+        x.stop()
+    if y is not None :
+        y.stop()
+    if z is not None :
+        z.stop()
+    if k is not None :
+        k.stop()
 
 @sio.on('response')
 def response(data):
     print(data)
-    
-    
-    
-    
+
+
+
+
+
+# 로컬 카메라 캡처 객체 생성
+cap = cv2.VideoCapture(0)  # 0은 기본 카메라를 의미합니다. 다른 카메라를 사용하려면 숫자를 변경하세요.
+
+# 서버 엔드포인트 URL 설정
+server_url = "http://3.36.70.207:5000/upload"  # 서버 URL을 적절하게 변경하세요.
+
+# 서버로 전송할 데이터 형식 설정 (이미지를 base64로 인코딩하여 전송)
+headers = {"Content-Type": "application/json"}
+
+# 이미지 크기와 압축 설정
+IMAGE_WIDTH = 640
+IMAGE_HEIGHT = 480
+JPEG_QUALITY = 80
+
+# 카메라 해상도 설정
+cap.set(3, IMAGE_WIDTH)
+cap.set(4, IMAGE_HEIGHT)
+
+
 # 이미지를 서버로 비동기로 전송하는 함수
 async def send_image_to_server_async(image_base64):
     async with aiohttp.ClientSession() as session:
@@ -196,7 +228,7 @@ async def capture_and_send():
 
         _, buffer = cv2.imencode(".jpg", frame, [int(cv2.IMWRITE_JPEG_QUALITY), JPEG_QUALITY])
         image_base64 = base64.b64encode(buffer).decode()
-        print(image_base64)
+        # print(image_base64)
         print(i,"success")
         i = i+1
 
@@ -212,137 +244,21 @@ async def capture_and_send():
 
 
 
-
-
-# # ----------------- 테스트2 --------------
-# @sio.on('camera_video')
-# def give_video(data):
-#     if data == "start":
-#         # take image, give to server
-#         print("start")
-#         good = """sudo mjpg_streamer -i 'input_uvc.so' -o 'output_http.so -w /usr/local/share/mjpg-streamer/www -p 9090'&"""
-#         os.system(good)
-#         time.sleep(2)
-        
-#         capt = asyncio.get_event_loop().run_until_complete(main())
-
-#     elif data == "end":
-#         # stop take image
-#         print("end")
-#         capt.cancel()
+# ----------------- 테스트2 --------------
+@sio.on('camera_video')
+def give_video(data):
+    if data == "start":
+        asyncio.run(capture_and_send())
+    elif data == "end":
+        # stop take image
+        print("end")
         
         
         
-# async def capture_and_encode_image():
-#     # 이미지 캡처
-#     capture = cv2.VideoCapture("http://127.0.0.1:9090/?action=stream")
-#     ret, frame = capture.read()
-#     frame = cv2.resize(frame, dsize=(int(320*1.8),int(240*1.8)), interpolation = cv2.INTER_AREA)
-
-#     capture.release()
-#     # 이미지 인코딩
-#     _, encoded_image = cv2.imencode('.jpg', frame)
-#     image_base64 = base64.b64encode(encoded_image).decode('utf-8')
-
-#     return image_base64
-
-
-# async def send_image_to_server(image_base64):
-#     print(image_base64)
-#     socketio.emit('upload',image_base64)
-
-
-
-
-
-# async def main():
-#     while True:
-#         try:
-#             # 이미지 캡처 및 인코딩 비동기로 처리
-#             image_base64 = await capture_and_encode_image()
-
-#             # 이미지 서버로 전송 비동기로 처리
-#             await send_image_to_server(image_base64)
-#             # 일정 시간 간격으로 반복
-#             await asyncio.sleep(1/60)
-
-
-#         except Exception as e:
-#             print("에러 발생:", str(e))
-
-
-
-# # ----------------- 테스트 2 end --------------
-
-
-
-
-
-
-# ----------------- 테스트1 --------------
-# @sio.on('camera_video')
-# def give_video(data):
-#     if data == "start":
-#         # take image, give to server
-#         print("start")
-#         good = """sudo mjpg_streamer -i 'input_uvc.so' -o 'output_http.so -w /usr/local/share/mjpg-streamer/www -p 9090'&"""
-#         os.system(good)
-#         time.sleep(2)
         
-#         capt = asyncio.get_event_loop().run_until_complete(main())
 
-#     elif data == "end":
-#         # stop take image
-#         print("end")
-#         capt.cancle()
-
-
-    
-# async def capture_and_encode_image():
-#     # 이미지 캡처
-#     capture = cv2.VideoCapture("http://127.0.0.1:9090/?action=stream")
-#     ret, frame = capture.read()
-#     frame = cv2.resize(frame, dsize=(int(320*1.8),int(240*1.8)), interpolation = cv2.INTER_AREA)
-
-#     capture.release()
-#     # 이미지 인코딩
-#     _, encoded_image = cv2.imencode('.jpg', frame)
-#     image_base64 = base64.b64encode(encoded_image).decode('utf-8')
-
-#     return image_base64
-
-# async def send_image_to_server(image_base64):
-#     url = "http://43.200.191.244:5000/upload"  # 실제 서버 주소와 포트를 입력하세요
-#     payload = {"image": image_base64}
-#     headers = {"Content-Type": "application/json"}
-
-#     response = await asyncio.get_event_loop().run_in_executor(
-#         None, lambda: requests.post(url, json=payload, headers=headers)
-#     )
-#     return response
-
-# async def main():
-#     while True:
-#         try:
-#             # 이미지 캡처 및 인코딩 비동기로 처리
-#             image_base64 = await capture_and_encode_image()
-
-#             # 이미지 서버로 전송 비동기로 처리
-#             response = await send_image_to_server(image_base64)
-
-#             if response.status_code == 200:
-#                 print("이미지 전송 성공")
-#             else:
-#                 print("이미지 전송 실패")
-
-#         except Exception as e:
-#             print("에러 발생:", str(e))
-
-#         # 일정 시간 간격으로 반복
-#         await asyncio.sleep(1/60)
 
 
 if __name__ == '__main__' :
+    gpio.setmode(gpio.BCM)
     sio.connect('http://3.36.70.207:5000')
-    asyncio.run(capture_and_send())
-    
--- server_total.py
+++ server_total.py
@@ -8,7 +8,12 @@ from flask_socketio import SocketIO, emit
 import pymysql
 from flask import Flask
 from flask_cors import CORS
-from flask import request
+from flask import request, jsonify
+import base64
+import websockets
+import cv2
+import numpy as np
+import asyncio
 from firebase_admin import messaging
 import firebase_admin
 from firebase_admin import credentials
@@ -344,6 +349,12 @@ def place1(pro_id):
     
     
     
+@app.route('/status/<string:sen>')
+def sensor(sen):
+    print(sen)
+    socketio.emit("sensor",sen)
+    
+    
 # ----------------- define -------------------- 
 
 # 보호자 회원가입 할 때 휠체어 사용자의 보호자 유무 확인
@@ -484,37 +495,73 @@ def video_conn(data):
     socketio.emit('camera_video',"end")
     
     
-    
-    
-# 테스트 필요
-    
-# 라즈베리파이에서 서버로 이미지 전송
-@socketio.on('upload')
-def rasp_to_server(data):
-    global data1
-    data1 = data
-    print("Test")
-    print(data)
-    socketio.emit('stream',data1,broadcast=True)
-    
 
+# 이미지 데이터를 읽어올 함수
+async def read_image_data():
+    # 이미지를 캡쳐하고 이미지 파일에서 읽어옴 (captured_image2.jpg는 최종으로 업데이트된 이미지 파일)
+    # 이 부분을 실제 이미지 캡쳐 코드로 대체해야 합니다.
+    with open("captured_image2.jpg", "rb") as image_file:
+        return image_file.read()
 
-    
-# 라즈베리파이에서 이미지 주는 경로
+# WebSocket 서버 설정
+async def image_server(websocket, path):
+    try:
+        while True:
+            # 이미지 데이터 읽기
+            image_data = await read_image_data()
+
+            # 이미지를 base64로 인코딩
+            image_base64 = base64.b64encode(image_data).decode()
+
+            # 클라이언트로 이미지 전송
+            await websocket.send(image_base64)
+
+            # 60프레임 주기로 이미지를 전송하기 위해 대기 (약 16ms 대기)
+            await asyncio.sleep(1 / 120)
+    except websockets.exceptions.ConnectionClosedOK:
+        pass
+
 @app.route('/upload', methods=['POST'])
-def handle_upload():
-    image = request.json['image']
-    # 이미지 처리 로직을 수행
-    # 안드로이드가 받을 수 있게
-    socketio.emit('stream',image,broadcast = True)
-    print("test")
-    return 200
+def upload_image():
+    try:
+        data = request.json
 
-    
-    
+        if 'image' in data:
+            # base64로 인코딩된 이미지를 디코딩하여 이미지 데이터로 변환
+            image_base64 = data['image']
+            image_data = base64.b64decode(image_base64)
+
+            # 이미지 데이터를 numpy 배열로 변환
+            image_np = np.frombuffer(image_data, dtype=np.uint8)
+            
+            # OpenCV를 사용하여 이미지로 변환
+            image = cv2.imdecode(image_np, cv2.IMREAD_COLOR)
+
+            # 이미지를 디스크에 저장 (예시: "captured_image2.jpg"로 저장)
+            cv2.imwrite("captured_image2.jpg", image)
+
+            return jsonify({"message": "이미지 업로드 및 저장 성공"}), 200
+        else:
+            return jsonify({"error": "이미지 데이터가 전송되지 않았습니다."}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+def run_websocket_server():
+    asyncio.set_event_loop(asyncio.new_event_loop())  # 이벤트 루프 생성
+    loop = asyncio.get_event_loop()
+    start_server = websockets.serve(image_server, "0.0.0.0", 8765)
+    loop.run_until_complete(start_server)
+    loop.run_forever()
+
 @app.route('/test/<string:value>')
 def test(value):
     return value
+
+
+
 if __name__ == '__main__':
     # app.run(host='0.0.0.0')
+    websocket_thread = threading.Thread(target=run_websocket_server)
+    websocket_thread.start()
     socketio.run(app, host='0.0.0.0',port = 5000)
+    
